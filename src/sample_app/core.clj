70171d18b61aafdc14a7778dc50b8ae4446cdcd6
Josue Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-06-02 09:38:17 -0300

    fix: create app with ring-handler

--- src/sample_app/core.clj
+++ src/sample_app/core.clj
@@ -3,16 +3,19 @@
   (:require [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]))
 
-(def routers
+;; https://github.com/metosin/reitit/blob/master/examples/ring-example/src/example/server.clj
+(def app
   "ring router app"
-  (ring/router
-   ["/" {:get (fn [_]
-                {:status 200, :body "http server running!"})}]
-   ["/ping" {:get (fn [_]
-                    {:status 200, :body "pong!"})}]))
+  (ring/ring-handler
+   (ring/router
+    ["/" {:get (fn [_]
+                 {:status 200, :body "http server running!"})}]
+    ["/ping" {:get (fn [_]
+                     {:status 200, :body "pong!"})}])
+   (ring/create-default-handler)))
 
 (defn -main
   "software entry point"
   [& _]
-  (jetty/run-jetty #'routers {:port 3000, :join? false})
+  (jetty/run-jetty #'app {:port 3000, :join? false})
   (println "server running in port 3000"))
